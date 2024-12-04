d19a83e84f78fe1d71ea0346c168bce2a55fc62f
vina123baov <vothaibao50@gmail.com>
2024-12-04 11:18:34 +0700

    up

--- app/src/main/java/com/example/myapplication/RoomDetailActivity.kt
+++ app/src/main/java/com/example/myapplication/RoomDetailActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
 
 class RoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
