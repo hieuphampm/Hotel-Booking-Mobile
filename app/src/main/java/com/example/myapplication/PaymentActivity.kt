3e74bed64a24afa3311e702aaf6ca4e19628185c
KhoaPR1991 <147786955+KhoaPR1991@users.noreply.github.com>
2024-12-13 15:56:16 +0700

    Update PaymentActivity.kt

--- app/src/main/java/com/example/myapplication/PaymentActivity.kt
+++ app/src/main/java/com/example/myapplication/PaymentActivity.kt
@@ -3,25 +3,29 @@ package com.example.myapplication
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class PaymentActivity : AppCompatActivity() {
 
+    private lateinit var tvPaymentAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
 
-     
-        val etAccountNumber: EditText = findViewById(R.id.card_number)
-        val etAccountName: EditText = findViewById(R.id.cardholder_name)
-        val etBankName: EditText = findViewById(R.id.payment_method_spinner)
-        val etPaymentAmount: EditText = findViewById(R.id.payment_amount)
-        val btnConfirmPayment: Button = findViewById(R.id.confirm_payment_button)
+  
+        val etAccountNumber: EditText = findViewById(R.id.et_account_number)
+        val etAccountName: EditText = findViewById(R.id.et_account_name)
+        val etBankName: EditText = findViewById(R.id.et_bank_name)
+        val btnConfirmPayment: Button = findViewById(R.id.btn_confirm_payment)
+        tvPaymentAmount = findViewById(R.id.tv_payment_amount)
 
-     
-        val paymentAmount = intent.getStringExtra("payment_amount") ?: "0"
-        etPaymentAmount.setText(paymentAmount)
+        val paymentAmount = intent.getStringExtra("room_price") ?: "0"
+
+        
+        tvPaymentAmount.text = "Amount: $paymentAmount"
 
         btnConfirmPayment.setOnClickListener {
             val accountNumber = etAccountNumber.text.toString().trim()
@@ -29,10 +33,9 @@ class PaymentActivity : AppCompatActivity() {
             val bankName = etBankName.text.toString().trim()
 
             if (accountNumber.isEmpty() || accountName.isEmpty() || bankName.isEmpty()) {
-                Toast.makeText(this, "Please complete the information", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Payment Successful!", Toast.LENGTH_SHORT).show()
-         
+                Toast.makeText(this, "Payment successful!", Toast.LENGTH_SHORT).show()
             }
         }
     }
